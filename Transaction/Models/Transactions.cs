7c86cb96edfc4a351fbea1abb21b4833d6412aa6
htooak2014 <38837363+htooak2014@users.noreply.github.com>
2023-08-17 20:33:17 +0630

    File UploadAdd project files.

---
+++ Transaction/Models/Transactions.cs
@@ -0,0 +1,13 @@
+﻿namespace Transaction.Models
+{
+    public class Transactions
+    {
+        public string TransactionId { get; set; } 
+        public DateTime TransactionDate { get; set; }
+        public Decimal Amount { get; set; } = 0.0m;
+
+        public string CurrencyCode { get; set; } = "";
+
+        public string Status { get; set; } = "";
+    }
+}
